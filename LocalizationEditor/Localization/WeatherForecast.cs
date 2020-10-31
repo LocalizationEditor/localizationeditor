8ef9ac6f04900ae20f6980346d5306b7e8fe4011
slukashov <slukashov@users.noreply.github.com>
2020-10-31 14:23:18 +0200

    Update WeatherForecast.cs

--- LocalizationEditor/Localization/WeatherForecast.cs
+++ LocalizationEditor/Localization/WeatherForecast.cs
@@ -4,7 +4,7 @@ namespace Localization
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; }   
 
         public int TemperatureC { get; set; }
 
