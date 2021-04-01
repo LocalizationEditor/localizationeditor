67c5ac57d3abbc966fa9e744dcd90fba57d2aed7
s.lukashov <s.lukashov@plarium.com>
2021-04-01 17:05:27 +0300

    IN-13282

---
+++ LocalizationEditor/LocalizationEditor.Admin/Configuration.cs
@@ -0,0 +1,32 @@
+using Autofac;
+using LocalizationEditor.Base.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using LocalizationEditor.Admin.Options;
+using LocalizationEditor.Admin;
+
+namespace LocalizationEditor.ConnectionStrings
+{
+  public class Configuration : Module, IConfigurationModule
+  {
+    public IConfiguration ConfigurationRoot { get; private set; }
+    public IHostingOption HostingOption { get; private set; }
+
+    public void AddOptions(IServiceCollection services)
+    {
+      services.Configure<PathOptions>(ConfigurationRoot.GetSection(nameof(PathOptions)));
+    }
+
+    public void SetConfig(IConfiguration configuration, IHostingOption hostingOption)
+    {
+      ConfigurationRoot = configuration;
+      HostingOption = hostingOption;
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<PathOptionsProvider>().As<IPathOptionsProvider>();
+      builder.RegisterType<UserService>().As<IUserService>();
+    }
+  }
+}
